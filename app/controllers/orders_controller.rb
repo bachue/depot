becf8b08e7decd90a92bef866de98b31146586fc
Bachue Zhou <bachue.shu@gmail.com>
2011-08-15 22:03:43 +0800

    finished paginate

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.xml
   def index
-    @orders = Order.all
+    @orders = Order.paginate :page => params[:page], :order => 'created_at desc', :per_page => 10
 
     respond_to do |format|
       format.html # index.html.erb
@@ -31,6 +31,7 @@ class OrdersController < ApplicationController
     end
 
     @order = Order.new
+    @not_display_layout_buttons = true
 
     respond_to do |format|
       format.html # new.html.erb
